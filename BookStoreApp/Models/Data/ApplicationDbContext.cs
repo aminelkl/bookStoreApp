095e5429cbb588ea0e83d3cff9fea855432f7811
amine <aminelakhalamine@hotmail.fr>
2023-03-19 23:11:46 -0400

    Added Category model and created database

---
+++ BookStoreApp/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
