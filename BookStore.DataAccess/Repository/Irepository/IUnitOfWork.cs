3931bc801a30a6ce74239a1e3c66d8af8650aaa7
amine <aminelakhalamine@hotmail.fr>
2023-04-03 15:52:05 -0400

    Added product to repository

--- BookStore.DataAccess/Repository/Irepository/IUnitOfWork.cs
+++ BookStore.DataAccess/Repository/Irepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace BookStore.DataAccess.Repository.Irepository
     {
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
+        IProductRepository Product { get; }
         void Save();
 
     }
