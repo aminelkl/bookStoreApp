4fc5998261897342ee48737284e12a98685c4802
amine <aminelakhalamine@hotmail.fr>
2023-04-04 17:06:42 -0400

    Finished product view, added sweetalert2, added productVM model

--- BookStoreApp/Areas/Admin/Controllers/ProductController.cs
+++ BookStoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -2,17 +2,21 @@
 using BookStore.DataAccess.Repository;
 using BookStore.DataAccess.Repository.Irepository;
 using BookStore.Models;
+using BookStore.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStoreApp.Controllers;
 [Area("Admin")]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -24,28 +28,57 @@ namespace BookStoreApp.Controllers;
        
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-            if(id== null || id == 0)
+
+        ProductVM productVM = new()
+        {
+            Product = new(),
+            CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+            CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            })
+        };
+
+        if (id== null || id == 0)
             {
                 //create product
-                return View(product);
+                //ViewBag.CategoryList = CategoryList;
+              //  ViewBag.CoverTypeList = CoverTypeList;
+                return View(productVM);
         }
             else
         {
                 //update product
         }
-        return View(product);
+        return View(productVM);
     }
 
     [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product obj)
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Update (obj);
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            if (file!=null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"images\products");
+                var extension = Path.GetExtension(file.FileName);
+                using (var fileStreams = new FileStream(Path.Combine(uploads,fileName+extension), FileMode.Create))
+                {
+                    file.CopyTo(fileStreams);
+                }
+                obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+            }
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
-                TempData["Success"] = "Product updated succesfully";
+                TempData["Success"] = "Product created succesfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
